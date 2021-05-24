7383da399c3132e390359919fa1d1ab525c9f205
canerkoseren <canerkoseren@gmail.com>
2021-05-25 00:19:13 +0300

    monday

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,11 @@
+import "./Expenses.css";
+
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 
 function Expenses(props) {
   return (
-    <div>
+    <Card className="expenses">
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -18,7 +21,7 @@ function Expenses(props) {
         amount={props.expenses[2].amount}
         date={props.expenses[2].date}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
